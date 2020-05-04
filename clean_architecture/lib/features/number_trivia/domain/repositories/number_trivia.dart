78196ad21077c4007853e1cc9cdb3dff4d46d1e5
Eric Omine <5587665-ericomine@users.noreply.gitlab.com>
2020-05-04 19:23:21 -0300

    entities/number_trivia.dart

--- clean_architecture/lib/features/number_trivia/domain/repositories/number_trivia.dart
+++
@@ -1,4 +0,0 @@
-abstract class NumberTriviaRepository {
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) async;
-  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() async;
-}
