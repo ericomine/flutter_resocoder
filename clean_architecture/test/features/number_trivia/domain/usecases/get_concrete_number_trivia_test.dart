73f476639faf70f35dc2a885d3a20ca92699c628
Eric Omine <5587665-ericomine@users.noreply.gitlab.com>
2020-05-04 19:57:35 -0300

    Created test file for GetConcreteNumberTrivia use case (create test before use case)

---
+++ clean_architecture/test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -0,0 +1,17 @@
+import 'package:clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
+
+void main() {
+  GetConcreteNumberTrivia usecase;
+  MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+}
