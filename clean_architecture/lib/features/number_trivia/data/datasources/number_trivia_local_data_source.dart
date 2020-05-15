2036e0781ef108abcb47486370ab65604f8d5330
ericomine <eric.omine@gmail.com>
2020-05-15 15:26:51 -0300

    Test fixture for cached data

--- clean_architecture/lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ clean_architecture/lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,3 +1,6 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:meta/meta.dart';
+
 import '../models/number_trivia_model.dart';
 
 abstract class NumberTriviaLocalDataSource {
@@ -5,4 +8,23 @@ abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
 
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  final SharedPreferences sharedPreferences;
+  NumberTriviaLocalDataSourceImpl({
+    @required this.sharedPreferences
+    });
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache) {
+    // TODO: implement cacheNumberTrivia
+    return null;
+  }
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    // TODO: implement getLastNumberTrivia
+    return null;
+  }
 }
