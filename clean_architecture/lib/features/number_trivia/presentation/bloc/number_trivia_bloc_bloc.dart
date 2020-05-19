c3d13a4c446bb4f6cdd25602d432b55854568a7d
ericomine <eric.omine@gmail.com>
2020-05-18 23:56:37 -0300

    Filling out NumberTriviaBlocEvent

--- clean_architecture/lib/features/number_trivia/presentation/bloc/number_trivia_bloc_bloc.dart
+++ clean_architecture/lib/features/number_trivia/presentation/bloc/number_trivia_bloc_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
 part 'number_trivia_bloc_event.dart';
 part 'number_trivia_bloc_state.dart';
--- clean_architecture/lib/features/number_trivia/presentation/bloc/number_trivia_bloc_event.dart
+++ clean_architecture/lib/features/number_trivia/presentation/bloc/number_trivia_bloc_event.dart
@@ -1,5 +1,15 @@
 part of 'number_trivia_bloc_bloc.dart';
 
 abstract class NumberTriviaBlocEvent extends Equatable {
-  const NumberTriviaBlocEvent();
+  NumberTriviaBlocEvent([List props = const <dynamic>[]]) : super(props);
 }
+
+class GetTriviaForConcreteNumber extends NumberTriviaBlocEvent {
+  final String numberString;
+
+  GetTriviaForConcreteNumber({
+    @required this.numberString
+    }) : super([numberString]);
+}
+
+class GetTriviaForRandomNumber extends NumberTriviaBlocEvent {}
